a0bcdaed587624c9fbbb3bb9e2ae51e0d18b584a
Sebastian M <ra3gan1991@gmail.com>
2022-02-23 13:06:01 +0100

    Added route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 require('dotenv').config()
 router.use('/user', require('./users'));
 router.use('/course', require('./courses'));
+router.use('/rol', require('./rol'));
 // router.use('', require(''));
 
 module.exports = router;
