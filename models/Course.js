8405eb3d844fc9d3a454ade87f122ffe9a1993d6
Sebastian M <ra3gan1991@gmail.com>
2022-02-23 12:21:34 +0100

    Added ROL model/manager

--- models/Course.js
+++ models/Course.js
@@ -1,17 +1,17 @@
 module.exports = class Course {
-    id;
-    title;
-    startDate;
-    endDate;
-    type;
+    #id;
+    #title;
+    #startDate;
+    #endDate;
+    #type;
 
     //Using destructuring to recieve the data.
     constructor({ id, title, startDate, endDate, type }) {
-        this.id = id;
-        this.title = title;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.type = type;
+        this.#id = id;
+        this.#title = title;
+        this.#startDate = startDate;
+        this.#endDate = endDate;
+        this.#type = type;
     }
 
 
