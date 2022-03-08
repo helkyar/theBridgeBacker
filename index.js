75be446df763c74e416f78d31169a425de1c843b
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-03-08 20:08:53 +0100

    Add jwt token

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = 3003;
 //Example importing middleware as a placeholder for future middewares
 const example1 = require("./middlewares").example1;
 app.use(require("express").json());
+
 app.use("/api", require("./routes"));
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,29 @@ const router = require("express").Router();
 require("dotenv").config();
 
 router.use("/login", require("./login"));
+
+/**
+ * JWT MIDDLEWARE AUTH
+ */
+// router.use(require("../middlewares/token"));
+
+/**
+ *  PROPUESTA DE VALIDACIÓN
+ */
+// const { schemaRegister } = require("../middlewares/validations");
+
+// router.post('/register', async (req, res) => {
+
+//     const { error } = schemaRegister.validate(req.body)
+
+//     if (error) {
+//         return res.status(400).json({error: error.details[0].message})
+//     }
+//     ...
+
 router.use("/course", require("./courses"));
 router.use("/user", require("./users"));
 router.use("/rol", require("./rol"));
 router.use("/class", require("./classes"));
-// router.use('', require(''));
 
 module.exports = router;
