a9757ae3012d687d7c4107a69c63ebc572784a78
Sebastian M <ra3gan1991@gmail.com>
2022-03-11 00:56:09 +0100

    Merge

--- managers/CourseManager.js
+++ managers/CourseManager.js
@@ -2,35 +2,33 @@ let Manager = require("./Manager");
 const Course = require("../models/Course");
 
 class CourseManager extends Manager {
-  static queries = {
-    getAll: "SELECT * FROM courses",
-    getCourse: "SELECT * FROM courses WHERE id=$1",
-    postCourse:
-      "INSERT INTO courses (title,startDate,endDate,type) VALUES ($1,$2,$3,$4) RETURNING *;", //Array course con course creado
-    patchCourse:
-      "UPDATE courses SET title = $1,startDate = $2,endDate = $3,type = $4 WHERE id = $5 RETURNING *", //Array course con course modificado
-    deleteCourse: "DELETE FROM courses WHERE id=$1", //Array course con course eliminado
-  };
+    static queries = {
+        getAll: "SELECT * FROM courses",
+        getCourse: "SELECT * FROM courses WHERE id=$1",
+        postCourse: "INSERT INTO courses (title,startdate,enddate,type) VALUES ($1,$2,$3,$4) RETURNING *;", //Array course con course creado
+        patchCourse: "UPDATE courses SET title = $1,startDate = $2,endDate = $3,type = $4 WHERE id = $5 RETURNING *", //Array course con course modificado
+        deleteCourse: "DELETE FROM courses WHERE id=$1", //Array course con course eliminado
+    };
 
-  static async getAllCourses() {
-    return await this.queryExec(this.queries.getAll, Course);
-  }
+    static async getAllCourses() {
+        return await this.queryExec(this.queries.getAll, Course);
+    }
 
-  static async getCourse({ id }) {
-    return await this.queryExec(this.queries.getCourse, Course, [id]);
-  }
+    static async getCourse({ id }) {
+        return await this.queryExec(this.queries.getCourse, Course, [id]);
+    }
 
-  static async postCourses({ title, startDate, endDate, type }) {
-    const params = [title, startDate, endDate, type];
-    return await this.queryExec(this.queries.postCourse, Course, params);
-  }
-  static async patchCourses({ id, title, startDate, endDate, type }) {
-    const params = [title, startDate, endDate, type, id];
-    return await this.queryExec(this.queries.patchCourse, Course, params);
-  }
-  static async deleteCourses({ id }) {
-    return await this.queryExec(this.queries.deleteCourse, Course, [id]);
-  }
+    static async postCourses({ title, startdate: startDate, enddate: endDate, type }) {
+        const params = [title, startDate, endDate, type];
+        return await this.queryExec(this.queries.postCourse, Course, params);
+    }
+    static async patchCourses({ id, title, startDate, endDate, type }) {
+        const params = [title, startDate, endDate, type, id];
+        return await this.queryExec(this.queries.patchCourse, Course, params);
+    }
+    static async deleteCourses({ id }) {
+        return await this.queryExec(this.queries.deleteCourse, Course, [id]);
+    }
 }
 
 module.exports = CourseManager;
--- managers/Manager.js
+++ managers/Manager.js
@@ -1,36 +1,36 @@
 const { Client } = require("pg");
 
 class Manager {
-  static clientParams = {
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DB,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
-  };
+    static clientParams = {
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DB,
+        password: process.env.PASSWORD,
+        port: process.env.PORT,
+    };
 
-  static async queryExec(query, model, params) {
-    let client = new Client(this.clientParams);
-    try {
-      client.connect();
-      let data = params
-        ? await client.query(query, params)
-        : await client.query(query);
-      let tempUsers = [];
-      // console.log(data);
-      data.rows.map((e) => {
-        tempUsers.push(new model(e));
-      });
-      // console.log(tempUsers);
-      return tempUsers;
-    } catch (error) {
-      console.log("Error en managerUser");
-      console.log(error);
-      console.log(query);
-      return undefined;
-    } finally {
-      client.end();
+    static async queryExec(query, model, params) {
+        let client = new Client(this.clientParams);
+        try {
+            client.connect();
+            let data = params ?
+                await client.query(query, params) :
+                await client.query(query);
+            let tempUsers = [];
+            console.log(data);
+            data.rows.map((e) => {
+                tempUsers.push(new model(e));
+            });
+            console.log(tempUsers);
+            return tempUsers;
+        } catch (error) {
+            console.log("Error en Query");
+            console.log(error);
+            console.log(query);
+            return undefined;
+        } finally {
+            client.end();
+        }
     }
-  }
 }
 module.exports = Manager;
