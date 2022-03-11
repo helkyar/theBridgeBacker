0ad197651144df65caf1762562a972a2f02274d5
Sebastian M <ra3gan1991@gmail.com>
2022-03-11 01:09:43 +0100

    final fix?

--- managers/UserManager.js
+++ managers/UserManager.js
@@ -2,25 +2,13 @@ let Manager = require("./Manager");
 let User = require("../models/User");
 
 class UserManager extends Manager {
-<<<<<<< HEAD
     static queries = {
         getAll: "SELECT * FROM users", // Array de users
         getUser: "SELECT * FROM users WHERE id=$1",
-        postUser: "INSERT INTO users (lastName,firstName,login,password,email) VALUES ($1,$2,$3,$4,$5) RETURNING *;", //Array user con user creado
+        postUser: "INSERT INTO users (lastname,firstname,login,password,email,rol) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;", //Array user con user creado
         patchUser: "UPDATE users SET lastName = $1,firstName = $2,login = $3,password = $4,email = $5 WHERE id = $6", //Array user con user modificado
         deleteUser: "DELETE FROM users WHERE id=$1", //Array user con user eliminado
     };
-=======
-  static queries = {
-    getAll: "SELECT * FROM users", // Array de users
-    getUser: "SELECT * FROM users WHERE id=$1",
-    postUser:
-      "INSERT INTO users (lastname,firstname,login,password,email,rol) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;", //Array user con user creado
-    patchUser:
-      "UPDATE users SET lastName = $1,firstName = $2,login = $3,password = $4,email = $5 WHERE id = $6", //Array user con user modificado
-    deleteUser: "DELETE FROM users WHERE id=$1", //Array user con user eliminado
-  };
->>>>>>> 53a8b8e79c4d66124c04c2fbd482c69a95c95f09
 
     static async getAllUsers() {
         return await this.queryExec(this.queries.getAll, User);
@@ -30,14 +18,8 @@ class UserManager extends Manager {
         return await this.queryExec(this.queries.getUser, User, [id]);
     }
 
-<<<<<<< HEAD
-    static async postUsers({ lastname, firstname, login, password, email }) {
-        const params = [lastname, firstname, login, password, email];
-=======
-  static async postUsers({ lastname, firstname, login, password, email, rol }) {
-    const params = [lastname, firstname, login, password, email, rol];
->>>>>>> 53a8b8e79c4d66124c04c2fbd482c69a95c95f09
-
+    static async postUsers({ lastname, firstname, login, password, email, rol }) {
+        const params = [lastname, firstname, login, password, email, rol];
         return await this.queryExec(this.queries.postUser, User, params);
     }
     static async patchUsers({ id, lastname, firstname, login, password, email }) {
