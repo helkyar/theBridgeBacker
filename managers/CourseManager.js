0a24302e6a109d951e4b3c9a6df27c651ece556c
Sebastian M <ra3gan1991@gmail.com>
2022-03-11 01:01:17 +0100

    fix merge

--- managers/CourseManager.js
+++ managers/CourseManager.js
@@ -2,7 +2,7 @@ let Manager = require("./Manager");
 const Course = require("../models/Course");
 
 class CourseManager extends Manager {
-<<<<<<< HEAD
+
     static queries = {
         getAll: "SELECT * FROM courses",
         getCourse: "SELECT * FROM courses WHERE id=$1",
@@ -10,17 +10,6 @@ class CourseManager extends Manager {
         patchCourse: "UPDATE courses SET title = $1,startDate = $2,endDate = $3,type = $4 WHERE id = $5 RETURNING *", //Array course con course modificado
         deleteCourse: "DELETE FROM courses WHERE id=$1", //Array course con course eliminado
     };
-=======
-  static queries = {
-    getAll: "SELECT * FROM courses",
-    getCourse: "SELECT * FROM courses WHERE id=$1",
-    postCourse:
-      "INSERT INTO courses (title,startdate,enddate,type) VALUES ($1,$2,$3,$4) RETURNING *;", //Array course con course creado
-    patchCourse:
-      "UPDATE courses SET title = $1,startDate = $2,endDate = $3,type = $4 WHERE id = $5 RETURNING *", //Array course con course modificado
-    deleteCourse: "DELETE FROM courses WHERE id=$1", //Array course con course eliminado
-  };
->>>>>>> 53a8b8e79c4d66124c04c2fbd482c69a95c95f09
 
     static async getAllCourses() {
         return await this.queryExec(this.queries.getAll, Course);
@@ -30,31 +19,17 @@ class CourseManager extends Manager {
         return await this.queryExec(this.queries.getCourse, Course, [id]);
     }
 
-<<<<<<< HEAD
-    static async postCourses({ title, startdate: startDate, enddate: endDate, type }) {
-        const params = [title, startDate, endDate, type];
+    static async postCourses({ title, startdate, enddate, type }) {
+        const params = [title, startdate, enddate, type];
         return await this.queryExec(this.queries.postCourse, Course, params);
     }
-    static async patchCourses({ id, title, startDate, endDate, type }) {
+    static async patchCourses({ id, title, startdate, enddate, type }) {
         const params = [title, startDate, endDate, type, id];
         return await this.queryExec(this.queries.patchCourse, Course, params);
     }
     static async deleteCourses({ id }) {
         return await this.queryExec(this.queries.deleteCourse, Course, [id]);
     }
-=======
-  static async postCourses({ title, startdate, enddate, type }) {
-    const params = [title, startdate, enddate, type];
-    return await this.queryExec(this.queries.postCourse, Course, params);
-  }
-  static async patchCourses({ id, title, startDate, endDate, type }) {
-    const params = [title, startDate, endDate, type, id];
-    return await this.queryExec(this.queries.patchCourse, Course, params);
-  }
-  static async deleteCourses({ id }) {
-    return await this.queryExec(this.queries.deleteCourse, Course, [id]);
-  }
->>>>>>> 53a8b8e79c4d66124c04c2fbd482c69a95c95f09
 }
 
 module.exports = CourseManager;
